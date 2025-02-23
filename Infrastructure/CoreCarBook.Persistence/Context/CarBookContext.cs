217c380876d26f648a0f42826340618f39951a2f
Orçun Aşık <asikorcun@gmail.com>
2025-02-23 21:52:14 +0300

    Created app context class

---
+++ Infrastructure/CoreCarBook.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,27 @@
+﻿using CoreCarBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCarBook.Persistence.Context;
+public class CarBookContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CoreCarBook;User ID=sa;Password=Password1;TrustServerCertificate=True;");
+    }
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Banner> Banners { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<CarDescription> CarDescriptions { get; set; }
+    public DbSet<CarFeature> CarFeatures { get; set; }
+    public DbSet<CarPricing> CarPricings { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<FooterAddress> FooterAddresses { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Pricing> Pricings { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+}
