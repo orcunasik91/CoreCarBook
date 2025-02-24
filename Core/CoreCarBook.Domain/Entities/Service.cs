47c23149f58a7839229605ef2c7275ae8f65a7ff
Orçun Aşık <asikorcun@gmail.com>
2025-02-24 22:35:19 +0300

    ServiceId property's name edited

--- Core/CoreCarBook.Domain/Entities/Service.cs
+++ Core/CoreCarBook.Domain/Entities/Service.cs
@@ -1,7 +1,7 @@
 ﻿namespace CoreCarBook.Domain.Entities;
 public class Service
 {
-    public int ServieId { get; set; }
+    public int ServiceId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string IconUrl { get; set; }
