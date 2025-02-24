00444c4517336004bd7372a027d057075af3aadd
Orçun Aşık <asikorcun@gmail.com>
2025-02-24 22:41:21 +0300

    Added dependency injection registrations

---
+++ Infrastructure/CoreCarBook.Persistence/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using CoreCarBook.Domain.Repositories;
+using CoreCarBook.Persistence.Context;
+using CoreCarBook.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreCarBook.Persistence;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+        services.AddDbContext<CarBookContext>();
+        services.AddScoped<ICarRepository, CarRepository>();
+        //services.AddScoped<ICarService, CarService>
+        return services;
+    }
+}
