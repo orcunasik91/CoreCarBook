0edad09e326a57779ba8a626f519297a180b0e1f
Orçun Aşık <asikorcun@gmail.com>
2025-02-24 22:42:59 +0300

    Implemented service interface created in the domain layer

---
+++ Application/CoreCarBook.Application/Services/CarService.cs
@@ -0,0 +1,39 @@
+﻿using CoreCarBook.Domain.Services;
+using CoreCarBook.Domain.Entities;
+using CoreCarBook.Domain.Repositories;
+
+namespace CoreCarBook.Application.Services;
+public class CarService(ICarRepository carRepository) : ICarService
+{
+    public async Task AddCarAsync(Car car)
+    {
+        await carRepository.AddAsync(car);
+        await carRepository.SaveChangesAsync();
+    }
+
+    public async Task DeleteCarAsync(int id)
+    {
+        var car = await carRepository.GetByIdAsync(id);
+        if(car is not null)
+        {
+            carRepository.Remove(car);
+            await carRepository.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<Car>> GetAllCarsAysnc()
+    {
+        return await carRepository.GetAllAsync();
+    }
+
+    public async Task<Car> GetCarByIdAsync(int id)
+    {
+        return await carRepository.GetByIdAsync(id);
+    }
+
+    public async Task UpdateCarAsync(Car car)
+    {
+        carRepository.Update(car);
+        await carRepository.SaveChangesAsync();
+    }
+}
