87fdac04e18c9b14ba9a5556ab03c5fb7679db37
Orçun Aşık <asikorcun@gmail.com>
2025-02-24 22:47:36 +0300

    Added service registrations from Application layer

--- Presentation/CoreCarBook.Api/Program.cs
+++ Presentation/CoreCarBook.Api/Program.cs
@@ -1,7 +1,10 @@
+using CoreCarBook.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
