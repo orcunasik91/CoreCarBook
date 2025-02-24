0896cb92a4b134676a290f201fcac560ed5ee146
Orçun Aşık <asikorcun@gmail.com>
2025-02-24 22:40:09 +0300

    Implemented repo interfaces created in the domain layer

---
+++ Infrastructure/CoreCarBook.Persistence/Repositories/CarRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoreCarBook.Domain.Entities;
+using CoreCarBook.Domain.Repositories;
+using CoreCarBook.Persistence.Context;
+
+namespace CoreCarBook.Persistence.Repositories;
+public class CarRepository : GenericRepository<Car>, ICarRepository
+{
+    public CarRepository(CarBookContext _context) : base(_context)
+    {
+    }
+}
---
+++ Infrastructure/CoreCarBook.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using CoreCarBook.Domain.Repositories;
+using CoreCarBook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCarBook.Persistence.Repositories;
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly CarBookContext context;
+    private readonly DbSet<T> dbSet;
+
+    public GenericRepository(CarBookContext _context)
+    {
+        context = _context;
+        dbSet = context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+
+    public async Task<List<T>> GetAllAsync() => await dbSet.ToListAsync();
+
+    public async Task<T> GetByIdAsync(int id) => await dbSet.FindAsync(id);
+
+    public void Remove(T entity) => dbSet.Remove(entity);
+
+    public async Task<int> SaveChangesAsync() => await context.SaveChangesAsync();
+
+    public void Update(T entity) => dbSet.Update(entity);
+}
